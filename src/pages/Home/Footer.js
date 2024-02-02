4ce11a8a9fcac0467f118ccc3f4cde5e194013ca
ihjas abdullah <ihjas.abdullah@acentura.com>
2024-02-02 14:26:11 +0530

    Added the footer

---
+++ src/pages/Home/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="py-10">
+      <div className="h-[2px] bg-tertiary w-full">
+
+      </div>
+      <div className="flex items-center justify-center flex-col mt-10">
+        <h1 className="text-white text-sm">Design & Developed by :</h1>
+        <h1 className="text-tertiary text-sm font-semibold">Ihjas</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
